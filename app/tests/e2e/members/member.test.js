9c933b02097b945d4ed7aff462f21d3077c108db
flemmingheidepedersen <fhp@magenta-aps.dk>
2016-12-16 11:57:53 +0100

    cleanup of spec

--- app/tests/e2e/members/member.test.js
+++ app/tests/e2e/members/member.test.js
@@ -9,34 +9,40 @@ var loginPage = require('../login/loginPage.po.js');
 describe('openESDH login page', function() {
 
 
-    //Executed before each of the "it" tests
-    beforeEach(function () {
+
+
+    it('should be able to remove a memeber', function() {
+
         loginPage.loginAsAdmin();
+
     });
 
-    //it('should be able to add a memeber', function() {
-    //
-    //
-    //    browser.get("http://localhost:8000/#/projekter/" + constants.PROJECT_NAME_1).then (function(response) {
-    //
-    //        addMemberPage.addMember(constants.PROJECT_NAME_1, constants.PROJECT_NAME_1_MEMBER1.name, constants.PROJECT_NAME_1_MEMBER1.role);
-    //
-    //        expect(addMemberPage.getMembers().getText()).toMatch(constants.PROJECT_NAME_1_MEMBER1.fullname);
-    //    });
-    //
-    //});
 
-    //it('should be able to remove a memeber', function() {
-    //
-    //
-    //    browser.get("http://localhost:8000/#/projekter/" + constants.PROJECT_NAME_1).then (function(response) {
-    //
-    //        removeMemberPage.removeMember(constants.PROJECT_NAME_1_MEMBER1.fullname);
-    //
-    //        expect(addMemberPage.getMembers().getText()).not.toMatch(constants.PROJECT_NAME_1_MEMBER1.fullname);
-    //    });
-    //
-    //});
+    it('should be able to remove a memeber', function() {
+
+
+        browser.get("http://localhost:8000/#/projekter/" + constants.PROJECT_NAME_1).then (function(response) {
+
+            removeMemberPage.removeMember(constants.PROJECT_NAME_1_MEMBER1.fullname);
+
+            expect(addMemberPage.getMembers().getText()).not.toMatch(constants.PROJECT_NAME_1_MEMBER1.fullname);
+        });
+
+    });
+
+    it('should be able to add a memeber', function() {
+
+
+        browser.get("http://localhost:8000/#/projekter/" + constants.PROJECT_NAME_1).then (function(response) {
+
+            addMemberPage.addMember(constants.PROJECT_NAME_1, constants.PROJECT_NAME_1_MEMBER1.name, constants.PROJECT_NAME_1_MEMBER1.role);
+
+            expect(addMemberPage.getMembers().getText()).toMatch(constants.PROJECT_NAME_1_MEMBER1.fullname);
+        });
+
+    });
+
+
 
     it('should be able to remove a memeber', function() {
 
