b3dbad019e75a5278856578fbb01de8b76c16986
flemmingheidepedersen <fhp@magenta-aps.dk>
2017-04-06 09:44:46 +0200

    testing

--- app/tests/e2e/conf.js
+++ app/tests/e2e/conf.js
@@ -3,11 +3,11 @@ var HtmlScreenshotReporter = require('protractor-jasmine2-screenshot-reporter');
 exports.config = {
     capabilities: {'browserName': 'chrome'},
 
-    chromeDriver:"/Users/flemmingheidepedersen/src/OpenDESK-UI/node_modules/protractor/node_modules/webdriver-manager/selenium/chromedriver_2.29",
+    //chromeDriver:"/Users/flemmingheidepedersen/src/OpenDESK-UI/node_modules/protractor/node_modules/webdriver-manager/selenium/chromedriver_2.29",
+    //
+    //seleniumServerJar: '/Users/flemmingheidepedersen/src/OpenDESK-UI/node_modules/protractor/node_modules/webdriver-manager/selenium/selenium-server-standalone-3.3.1.jar',
 
-    seleniumServerJar: '/Users/flemmingheidepedersen/src/OpenDESK-UI/node_modules/protractor/node_modules/webdriver-manager/selenium/selenium-server-standalone-3.3.1.jar',
 
-    baseUrl: 'http://localhost:9000/', 
     /*multiCapabilities: [
      {'browserName': 'chrome'},
      {'browserName': 'firefox'},
@@ -36,6 +36,8 @@ exports.config = {
             browser.capabilities = capabilities;
         });
 
+        require('ts-node').register({ project: 'e2e' });
+
         browser.params.loginDetails = (function () {
             // Load the username/password to use from a config file located
             // in the parent directory of the OpenESDH-UI root
@@ -45,6 +47,9 @@ exports.config = {
                 return {'username': 'admin', 'password': 'bullerfisk1992'};
             }
         })();
+
+
+
         // Add a screenshot reporter and store screenshots to `/tmp/screnshots`:
         //see https://github.com/mlison/protractor-jasmine2-screenshot-reporter/issues/4
         /*jasmine.getEnv().addReporter(
